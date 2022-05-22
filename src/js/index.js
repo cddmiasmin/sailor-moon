ef1d99f7c77bd011bd517648c7c75aa05b805e5a
Iasmin Dias <iasmin.dias87@gmail.com>
2022-05-22 19:02:18 -0300

    DESAFIO: Fazer o Jogador2 deixar de ser estático

--- src/js/index.js
+++ src/js/index.js
@@ -1,50 +1,65 @@
-/* 
- OBJETIVO 1 - quando passar o mouse em cima do personagem temos que:
-
-  - colocar a classe selecionado no personagem que passamos o mouse em cima para adicionar a animação nele 
-
- - retirar a classe selecionado do personagem que está selecionado
 
 
- OBJETIVO 2 - quando passar o mouse em cima do personagem, trocar a imagem e nome do personagem grande
-
-    - alterar a imagem do jogador 1
-    - alterar o nome do jogador 1
-*/
+var escolherJogador = new Boolean(true);
 
 const personagens = document.querySelectorAll(".personagem");
 
 personagens.forEach((personagem) => {
-  personagem.addEventListener("mouseenter", () => {
 
-    const idSelecionado = personagem.attributes.id.value;
+    personagem.addEventListener("mouseenter", () => {
+      
+      const QuemJogador1 = document.querySelector('.jogador-1-selecionado');
+      const QuemJogador2 = document.querySelector('.jogador-2-selecionado');
 
-    if(idSelecionado === 'tuxedo-kamen') return;
+        if (escolherJogador == true)
+        { 
+            const idSelecionado = personagem.attributes.id.value;
 
-    /* 
-        OBJETIVO 1 - quando passar o mouse em cima do personagem temos que:
-
-        - colocar a classe selecionado no personagem que passamos o mouse em cima para adicionar a animação nele 
-
-        - retirar a classe selecionado do personagem que está selecionado
-    */
-    const personagemSelecionado = document.querySelector(".selecionado");
-    personagemSelecionado.classList.remove("selecionado");
-
-    personagem.classList.add("selecionado");
-
-    /* 
-        OBJETIVO 2 - quando passar o mouse em cima do personagem da listagem, trocar a imagem e nome do personagem grande
-            - alterar a imagem do jogador 1
-            - alterar o nome do jogador 1
-    */
-
-    const imagemJogador1 = document.getElementById('personagem-jogador-1');
-    imagemJogador1.src = `./src/imagens/big-${idSelecionado}.png`;
-
-    const nomeJogador1 = document.getElementById('nome-jogador-1');
-    const nomeSelecionado = personagem.getAttribute('data-name');
+            if(idSelecionado === QuemJogador2.attributes.id.value ) return;
     
-    nomeJogador1.innerHTML = nomeSelecionado;
-  });
+            const personagemSelecionado = document.querySelector(".jogador-1-selecionado");
+            personagemSelecionado.classList.remove("jogador-1-selecionado");
+    
+            personagem.classList.add("jogador-1-selecionado");
+    
+            const imagemJogador1 = document.getElementById('personagem-jogador-1');
+            imagemJogador1.src = `./src/imagens/big-${idSelecionado}.png`;
+          
+            const mudarNumPlayer = document.getElementById(`player-${idSelecionado}`);
+            mudarNumPlayer.textContent = "1P";
+
+            const nomeJogador1 = document.getElementById('nome-jogador-1');
+            const nomeSelecionado = personagem.getAttribute('data-name');
+            
+            nomeJogador1.innerHTML = nomeSelecionado;
+
+        }
+        else {
+          const idSelecionado = personagem.attributes.id.value;
+
+          if(idSelecionado === QuemJogador1.attributes.id.value ) return;
+  
+          const personagemSelecionado = document.querySelector(".jogador-2-selecionado");
+          personagemSelecionado.classList.remove("jogador-2-selecionado");
+  
+          personagem.classList.add("jogador-2-selecionado");
+  
+          const imagemJogador2 = document.getElementById('personagem-jogador-2');
+          imagemJogador2.src = `./src/imagens/big-${idSelecionado}.png`;
+
+          const mudarNumPlayer2 = document.getElementById(`player-${idSelecionado}`);
+          mudarNumPlayer2.textContent = "2P";
+        
+          const nomeJogador2 = document.getElementById('nome-jogador-2');
+          const nomeSelecionado = personagem.getAttribute('data-name');
+          
+          nomeJogador2.innerHTML = nomeSelecionado;
+        }
+    });
 });
+
+function ConteudoRecebeuClick()
+{
+    if(escolherJogador == true) escolherJogador = false;
+    else escolherJogador = true
+}
